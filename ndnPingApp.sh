02818590d88ba9f9e65057f2b8ca758dfe58f673
amanmaldar <amanmaldar@gmail.com>
2018-09-20 15:49:49 -0400

    automate the pinging for ndnping

---
+++ ndnPingApp.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "start the pings"
+ndnping /ndn/d-site/d -i 1 -c 5000 -n 1 > /home/lenovo/Dropbox/Thesis/Logs/minindn3/clientLogs.txt 
+ndnping /ndn/d-site/d -i 1 -c 5000 -n 6001 > /home/lenovo/Dropbox/Thesis/Logs/minindn3/clientLogs.txt 
+ndnping /ndn/d-site/d -i 1 -c 10000 -n 15001 > /home/lenovo/Dropbox/Thesis/Logs/minindn3/clientLogs.txt 
+ndnping /ndn/d-site/d -i 1 -c 20000 -n 30001 > /home/lenovo/Dropbox/Thesis/Logs/minindn3/clientLogs.txt 
+ndnping /ndn/d-site/d -i 1 -c 25000 -n 60001 > /home/lenovo/Dropbox/Thesis/Logs/minindn3/clientLogs.txt 
+ndnping /ndn/d-site/d -i 1 -c 5000 -n 100001 > /home/lenovo/Dropbox/Thesis/Logs/minindn3/clientLogs.txt 
+ndnping /ndn/d-site/d -i 1 -c 5000 -n 110001 > /home/lenovo/Dropbox/Thesis/Logs/minindn3/clientLogs.txt 
+ndnping /ndn/d-site/d -i 1 -c 5000 -n 120001 > /home/lenovo/Dropbox/Thesis/Logs/minindn3/clientLogs.txt 
+echo "done"
