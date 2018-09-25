ad305057fed3c016dee5c7d8b083b03ff6f8b25b
Aman Maldar <amanmaldar@users.noreply.github.com>
2018-09-24 23:07:52 -0400

    Update ndnPingApp.sh
    fixed i = 0

--- ndnPingApp.sh
+++ ndnPingApp.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 echo "Starting the pings"
-ndnping /ndn/d-site/d -i 1 -c 5000 -n 1 > /home/lenovo/Dropbox/Thesis/Logs/minindn3/clientLogs.txt 
-ndnping /ndn/d-site/d -i 1 -c 5000 -n 1 > /home/lenovo/Dropbox/Thesis/Logs/minindn3/clientLogs.txt 
-echo "Finshed 1/8"
-ndnping /ndn/d-site/d -i 1 -c 5000 -n 6001 > /home/lenovo/Dropbox/Thesis/Logs/minindn3/clientLogs.txt 
-ndnping /ndn/d-site/d -i 1 -c 5000 -n 6001 > /home/lenovo/Dropbox/Thesis/Logs/minindn3/clientLogs.txt 
-echo "Finshed 2/8"
-ndnping /ndn/d-site/d -i 1 -c 10000 -n 15001 > /home/lenovo/Dropbox/Thesis/Logs/minindn3/clientLogs.txt 
-ndnping /ndn/d-site/d -i 1 -c 10000 -n 15001 > /home/lenovo/Dropbox/Thesis/Logs/minindn3/clientLogs.txt 
-echo "Finshed 3/8"
-ndnping /ndn/d-site/d -i 1 -c 20000 -n 30001 > /home/lenovo/Dropbox/Thesis/Logs/minindn3/clientLogs.txt 
-ndnping /ndn/d-site/d -i 1 -c 20000 -n 30001 > /home/lenovo/Dropbox/Thesis/Logs/minindn3/clientLogs.txt 
-echo "Finshed 4/8"
-ndnping /ndn/d-site/d -i 1 -c 25000 -n 60001 > /home/lenovo/Dropbox/Thesis/Logs/minindn3/clientLogs.txt 
-ndnping /ndn/d-site/d -i 1 -c 25000 -n 60001 > /home/lenovo/Dropbox/Thesis/Logs/minindn3/clientLogs.txt 
-echo "Finshed 5/8"
-ndnping /ndn/d-site/d -i 1 -c 5000 -n 100001 > /home/lenovo/Dropbox/Thesis/Logs/minindn3/clientLogs.txt 
-ndnping /ndn/d-site/d -i 1 -c 5000 -n 100001 > /home/lenovo/Dropbox/Thesis/Logs/minindn3/clientLogs.txt 
-echo "Finshed 6/8"
-ndnping /ndn/d-site/d -i 1 -c 5000 -n 110001 > /home/lenovo/Dropbox/Thesis/Logs/minindn3/clientLogs.txt 
-ndnping /ndn/d-site/d -i 1 -c 5000 -n 110001 > /home/lenovo/Dropbox/Thesis/Logs/minindn3/clientLogs.txt 
-echo "Finshed 7/8"
-ndnping /ndn/d-site/d -i 1 -c 5000 -n 120001 > /home/lenovo/Dropbox/Thesis/Logs/minindn3/clientLogs.txt 
-ndnping /ndn/d-site/d -i 1 -c 5000 -n 120001 > /home/lenovo/Dropbox/Thesis/Logs/minindn3/clientLogs.txt 
-echo "Finshed 100%"
+ndnping /ndn/d-site/d -c 5000 -n 1 > /home/lenovo/Dropbox/Thesis/Logs/minindn3/clientLogs.txt 
+ndnping /ndn/d-site/d -c 5000 -n 1 > /home/lenovo/Dropbox/Thesis/Logs/minindn3/clientLogs.txt 
+echo "Finished 1/8"
+ndnping /ndn/d-site/d -c 5000 -n 6001 > /home/lenovo/Dropbox/Thesis/Logs/minindn3/clientLogs.txt 
+ndnping /ndn/d-site/d -c 5000 -n 6001 > /home/lenovo/Dropbox/Thesis/Logs/minindn3/clientLogs.txt 
+echo "Finished 2/8"
+ndnping /ndn/d-site/d -c 10000 -n 15001 > /home/lenovo/Dropbox/Thesis/Logs/minindn3/clientLogs.txt 
+ndnping /ndn/d-site/d -c 10000 -n 15001 > /home/lenovo/Dropbox/Thesis/Logs/minindn3/clientLogs.txt 
+echo "Finished 3/8"
+ndnping /ndn/d-site/d -c 20000 -n 30001 > /home/lenovo/Dropbox/Thesis/Logs/minindn3/clientLogs.txt 
+ndnping /ndn/d-site/d -c 20000 -n 30001 > /home/lenovo/Dropbox/Thesis/Logs/minindn3/clientLogs.txt 
+echo "Finished 4/8"
+ndnping /ndn/d-site/d -c 25000 -n 60001 > /home/lenovo/Dropbox/Thesis/Logs/minindn3/clientLogs.txt 
+ndnping /ndn/d-site/d -c 25000 -n 60001 > /home/lenovo/Dropbox/Thesis/Logs/minindn3/clientLogs.txt 
+echo "Finished 5/8"
+ndnping /ndn/d-site/d -c 5000 -n 100001 > /home/lenovo/Dropbox/Thesis/Logs/minindn3/clientLogs.txt 
+ndnping /ndn/d-site/d -c 5000 -n 100001 > /home/lenovo/Dropbox/Thesis/Logs/minindn3/clientLogs.txt 
+echo "Finished 6/8"
+ndnping /ndn/d-site/d -c 5000 -n 110001 > /home/lenovo/Dropbox/Thesis/Logs/minindn3/clientLogs.txt 
+ndnping /ndn/d-site/d -c 5000 -n 110001 > /home/lenovo/Dropbox/Thesis/Logs/minindn3/clientLogs.txt 
+echo "Finished 7/8"
+ndnping /ndn/d-site/d -c 5000 -n 120001 > /home/lenovo/Dropbox/Thesis/Logs/minindn3/clientLogs.txt 
+ndnping /ndn/d-site/d -c 5000 -n 120001 > /home/lenovo/Dropbox/Thesis/Logs/minindn3/clientLogs.txt 
+echo "Finished 100%"
 echo "Get the results"
